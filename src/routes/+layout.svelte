2e23ff36297d204cb84e5934f0b600d873a9b823
Matej Bačo <matejbaco2000@gmail.com>
2023-05-18 10:05:19 +0200

    Prepare UI with Pink

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '@appwrite.io/pink';
+	import '@appwrite.io/pink-icons';
+</script>
+
+<div class="flex flex-vertical">
+	<header class="main-header u-padding-inline-end-0">
+		<a class="logo eyebrow-heading-2" href="/"> Auth UI </a>
+		<div class="main-header-end u-margin-inline-end-16">
+			<ul class="buttons-list is-with-padding">
+				<li class="buttons-list-item u-padding-inline-0">
+					<button class="button is-secondary"><span aria-hidden="true" class="icon-star"></span><span class="text">Star on GitHub</span></button>
+				</li>
+			</ul>
+		</div>
+	</header>
+	<div class="main-content">
+		<slot />
+	</div>
+</div>
