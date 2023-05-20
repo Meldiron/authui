77a10fcbdb252f0c763cdc037f4cb507bc0a0682
Matej Bačo <matejbaco2000@gmail.com>
2023-05-20 18:32:51 +0200

    Update sitemap generation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,12 @@
 </script>
 
 <slot />
+
+
+<div style="display: none;" id="sitemap-links">
+	<a href="/">Home</a>
+	<a href="/sign-up">Sign Up</a>
+	<a href="/sms-login">Phone Login</a>
+	<a href="/email-login">Email Login</a>
+	<a href="/forgot-password">Password Recovery</a>
+</div>
